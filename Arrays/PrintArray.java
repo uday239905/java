758496ae4e46a0ebe486d85af79a82c2a5184aaf
uday <budaykiran001@gmail.com>
2024-09-04 22:09:30 +0530

    added printarray

---
+++ Arrays/PrintArray.java
@@ -0,0 +1,24 @@
+package Arrays;
+import java.util.Scanner;
+public class PrintArray {
+    public static void main(String[] args) {
+        
+    
+    
+    Scanner sc=new Scanner(System.in);
+        System.out.println("enter array size");
+    int size=sc.nextInt();
+    int [] a=new int[size];
+    
+    for(int i=0;i<=a.length-1;i++)
+    {
+        System.out.println("enter array elements:"+i);
+        a[i]=sc.nextInt();
+    }
+    for(int i=0;i<=a.length-1;i++)
+    {
+        System.out.print(a[i]+",");
+    }
+}
+
+}
