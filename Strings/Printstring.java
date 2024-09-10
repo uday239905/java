e911ebfdd3eb026205e941fdf8f0c74a52f10cc4
uday <budaykiran001@gmail.com>
2024-09-10 15:37:07 +0530

    printstring

---
+++ Strings/Printstring.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class PrintString{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a string");
+        String s=sc.nextLine();
+            
+        System.out.print(s);
+        
+    }
+}
